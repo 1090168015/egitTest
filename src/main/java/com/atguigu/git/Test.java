ef3f9e28585a977a9c94a123e99bb6161feba990
shenxiang <shenx186@163.com>
2021-01-23 23:15:59 +0800

    111

--- src/main/java/com/atguigu/git/Test.java
+++ src/main/java/com/atguigu/git/Test.java
@@ -1,8 +1,8 @@
 package com.atguigu.git;
 
 public class Test {
-	public void test1() {}
-	public void testMaster() {}//主分支修改Test类添加testMaster方法
-	public void testDev() {}//dev分支创建方法
+    public void test111() {}
+    public void testMaster() {}//主分支修改Test类添加testMaster方法
+    public void testDev() {}//dev分支创建方法
 
 }
